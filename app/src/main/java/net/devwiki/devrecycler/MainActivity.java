34069259a76017e2193fdd309f436554c26292ba
Asia <bozhou2010@live.com>
2017-05-07 19:21:19 +0800

    添加Demo

--- app/src/main/java/net/devwiki/devrecycler/MainActivity.java
+++ app/src/main/java/net/devwiki/devrecycler/MainActivity.java
@@ -2,12 +2,38 @@ package net.devwiki.devrecycler;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView studentRv;
+    private StudentAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        studentRv = (RecyclerView) findViewById(R.id.student_rv);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        studentRv.setLayoutManager(layoutManager);
+        mAdapter = new StudentAdapter(this);
+        View headerView = LayoutInflater.from(this).inflate(R.layout.header_student, studentRv, false);
+        mAdapter.addHeaderView(headerView);
+        studentRv.setAdapter(mAdapter);
+        List<Student> list = new ArrayList<>();
+        for (int i = 0; i < 50; i++) {
+            Student student = new Student();
+            student.setName("学生" + i);
+            student.setAge(i);
+            list.add(student);
+        }
+        mAdapter.fillList(list);
     }
 }
---
+++ app/src/main/java/net/devwiki/devrecycler/Student.java
@@ -0,0 +1,36 @@
+package net.devwiki.devrecycler;
+
+/**
+ * 学生类
+ * Created by Asia on 2017/5/7.
+ */
+
+public class Student {
+
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
